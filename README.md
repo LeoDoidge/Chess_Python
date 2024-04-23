8ab6a69412e935466100b3b0e317f7355313dd74
leo <leo.doidge@gmail.com>
2024-04-23 14:57:25 +0200

    Complete arborescence overhaul

--- README.md
+++
@@ -1,4 +0,0 @@
-# Chess_python
-To run the game must cd into folder "Chess_python" first if running on VSCode on Chromebook
-DO NOT CHANGE IMAGE NAME
-
--- globals.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-W = 500
-H = 500
-board_size = 8
-square_size = W // board_size
-backround_color = 255, 255, 255
-screen = pygame.display.set_mode([W, H])
